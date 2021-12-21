9ee571479ecb4458b541902312e6e6a77f9ca8bb
KOLTAKOV Aleksey <Aleksey.KOLTAKOV@raiffeisen.ru>
2021-12-22 02:30:42 +0300

    Fix protocol settings

--- src/main/g8/src/main/scala/$package__packaged$/settings/Protocol.scala
+++ src/main/g8/src/main/scala/$package__packaged$/settings/Protocol.scala
@@ -13,6 +13,6 @@ object Protocol {
       .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
       .acceptLanguageHeader("en-US,en;q=0.5")
       .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0")
-      .warmUp( serviceUrl + "/version" )
+      .warmUp( baseUrl + "/version" )
   }
 }
